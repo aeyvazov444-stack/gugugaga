9e68e72b9dff7de4c1e9473e1da4981c5c38d789
lenovo <aeyvazov444@gmail.com>
2026-03-01 10:30:47 +0400

    commit 6

--- main.py
+++ main.py
@@ -1,264 +1,314 @@
-# import random
+# # import random
+# #
+# #
+# # class Human:
+# #     def __init__(self, name="Human", job=None, home=None, car=None):
+# #         self.name = name
+# #         self.money = 100
+# #         self.gladness = 50
+# #         self.satiety = 50
+# #         self.job = job
+# #         self.car = car
+# #         self.home = home
+# #
+# #     def get_home(self):
+# #         self.home = House()
+# #
+# #     def get_car(self):
+# #         self.car = Auto(brands_of_car)
+# #
+# #     def get_job(self):
+# #         if self.car.drive():
+# #             self.job = Job(job_list)
+# #         else:
+# #             self.to_repair()
+# #
+# #     def eat(self):
+# #         if self.home.food <= 0:
+# #             self.shopping("food")
+# #         else:
+# #             if self.satiety >= 100:
+# #                 self.satiety = 100
+# #                 return
+# #             self.satiety += 5
+# #             self.home.food -= 5
+# #
+# #     def work(self):
+# #         if self.car.drive():
+# #             self.money += self.job.salary
+# #             self.gladness -= self.job.gladness_less
+# #             self.satiety -= 4
+# #         else:
+# #             if self.car.fuel < 20:
+# #                 self.shopping("fuel")
+# #             else:
+# #                 self.to_repair()
+# #
+# #     def shopping(self, manage):
+# #         if not self.car.drive():
+# #             if self.car.fuel < 20:
+# #                 manage = "fuel"
+# #             else:
+# #                 self.to_repair()
+# #                 return
+# #
+# #         if manage == "fuel":
+# #             print("I bought fuel")
+# #             self.money -= 100
+# #             self.car.fuel += 100
+# #         elif manage == "food":
+# #             print("Bought food")
+# #             self.money -= 50
+# #             self.home.food += 50
+# #         elif manage == "delicacies":
+# #             print("Hooray! Delicious!")
+# #             self.gladness += 10
+# #             self.satiety += 2
+# #             self.money -= 15
+# #
+# #     def chill(self):
+# #         self.gladness += 10
+# #         self.home.mess += 5
+# #
+# #     def clean_home(self):
+# #         self.gladness -= 5
+# #         self.home.mess = 0
+# #
+# #     def to_repair(self):
+# #         print("Repairing car")
+# #         self.car.strength += 100
+# #         self.money -= 50
+# #
+# #     def days_indexes(self, day):
+# #         day_text = f" Today the {day} day of {self.name}'s life "
+# #         print(f"{day_text:=^50}")
+# #
+# #         print(f"{self.name}'s indexes".center(50))
+# #         print(f"Money: {self.money}")
+# #         print(f"Satiety: {self.satiety}")
+# #         print(f"Gladness: {self.gladness}")
+# #
+# #         print("Home indexes".center(50))
+# #         print(f"Food: {self.home.food}")
+# #         print(f"Mess: {self.home.mess}")
+# #
+# #         print(f"{self.car.brand} car indexes".center(50))
+# #         print(f"Fuel: {self.car.fuel}")
+# #         print(f"Strength: {self.car.strength}")
+# #         print()
+# #
+# #     def is_alive(self):
+# #         if self.gladness < 0:
+# #             print("Depression…")
+# #             return False
+# #         if self.satiety < 0:
+# #             print("Dead…")
+# #             return False
+# #         if self.money < -500:
+# #             print("Bankrupt…")
+# #             return False
+# #         return True
+# #
+# #     def live(self, day):
+# #         if not self.is_alive():
+# #             return False
+# #
+# #         dice = random.randint(1, 4)
+# #
+# #         if self.home is None:
+# #             print("Settled in the house")
+# #             self.get_home()
+# #
+# #         if self.car is None:
+# #             self.get_car()
+# #             print(f"I bought a car {self.car.brand}")
+# #
+# #         if self.job is None:
+# #             self.get_job()
+# #             print(
+# #                 f"I don't have a job, going to get a job "
+# #                 f"{self.job.job} with salary {self.job.salary}"
+# #             )
+# #
+# #         self.days_indexes(day)
+# #
+# #         if self.satiety < 20:
+# #             print("I'll go eat")
+# #             self.eat()
+# #         elif self.gladness < 20:
+# #             if self.home.mess > 15:
+# #                 print("Too much mess, cleaning...")
+# #                 self.clean_home()
+# #             else:
+# #                 print("Let`s chill!")
+# #                 self.chill()
+# #         elif self.money < 0:
+# #             print("Start working")
+# #             self.work()
+# #         elif self.car.strength < 15:
+# #             print("I need to repair my car")
+# #             self.to_repair()
+# #         elif dice == 1:
+# #             print("Let`s chill!")
+# #             self.chill()
+# #         elif dice == 2:
+# #             print("Start working")
+# #             self.work()
+# #         elif dice == 3:
+# #             print("Cleaning time!")
+# #             self.clean_home()
+# #         elif dice == 4:
+# #             print("Time for treats!")
+# #             self.shopping("delicacies")
+# #
+# #
+# # class Auto:
+# #     def __init__(self, brand_list):
+# #         self.brand = random.choice(list(brand_list))
+# #         self.fuel = brand_list[self.brand]["fuel"]
+# #         self.strength = brand_list[self.brand]["strength"]
+# #         self.consumption = brand_list[self.brand]["consumption"]
+# #
+# #     def drive(self):
+# #         if self.strength > 0 and self.fuel >= self.consumption:
+# #             self.fuel -= self.consumption
+# #             self.strength -= 1
+# #             return True
+# #         else:
+# #             print("The car cannot move")
+# #             return False
+# #
+# #
+# # class House:
+# #     def __init__(self):
+# #         self.mess = 0
+# #         self.food = 0
+# #
+# #
+# # class Job:
+# #     def __init__(self, job_list):
+# #         self.job = random.choice(list(job_list))
+# #         self.salary = job_list[self.job]["salary"]
+# #         self.gladness_less = job_list[self.job]["gladness_less"]
+# #
+# #
+# # job_list = {
+# #     "Java developer": {"salary": 50, "gladness_less": 10},
+# #     "Python developer": {"salary": 40, "gladness_less": 3},
+# #     "C++ developer": {"salary": 45, "gladness_less": 25},
+# #     "Rust developer": {"salary": 70, "gladness_less": 1},
+# # }
+# #
+# # brands_of_car = {
+# #     "BMW": {"fuel": 100, "strength": 100, "consumption": 6},
+# #     "Lada": {"fuel": 50, "strength": 40, "consumption": 10},
+# #     "Volvo": {"fuel": 70, "strength": 150, "consumption": 8},
+# #     "Ferrari": {"fuel": 80, "strength": 120, "consumption": 14},
+# # }
+# #
+# # nick = Human(name="Nick")
+# #
+# # for day in range(1, 8):
+# #     if not nick.live(day):
+# #         break
+# # Если библиотека не установлена, раскомментируй строку ниже
+# # pip install colorama
+#
+# import colorama
+# import inspect
+# from colorama import init, Fore, Back, Style
+#
+# init(autoreset=True)
+#
+# print("=" * 50)
+# print("ИНТРОСПЕКЦИЯ БИБЛИОТЕКИ COLORAMA")
+# print("=" * 50)
 #
 #
-# class Human:
-#     def __init__(self, name="Human", job=None, home=None, car=None):
-#         self.name = name
-#         self.money = 100
-#         self.gladness = 50
-#         self.satiety = 50
-#         self.job = job
-#         self.car = car
-#         self.home = home
+# print("\nДокументация модуля:")
+# print(colorama.__doc__)
 #
-#     def get_home(self):
-#         self.home = House()
+# print("\nОсновные атрибуты colorama:")
+# print(dir(colorama))
 #
-#     def get_car(self):
-#         self.car = Auto(brands_of_car)
+# print("\nИнформация о Fore:")
+# print(dir(Fore))
 #
-#     def get_job(self):
-#         if self.car.drive():
-#             self.job = Job(job_list)
-#         else:
-#             self.to_repair()
+# print("\nИнформация о Back:")
+# print(dir(Back))
 #
-#     def eat(self):
-#         if self.home.food <= 0:
-#             self.shopping("food")
-#         else:
-#             if self.satiety >= 100:
-#                 self.satiety = 100
-#                 return
-#             self.satiety += 5
-#             self.home.food -= 5
+# print("\nИнформация о Style:")
+# print(dir(Style))
 #
-#     def work(self):
-#         if self.car.drive():
-#             self.money += self.job.salary
-#             self.gladness -= self.job.gladness_less
-#             self.satiety -= 4
-#         else:
-#             if self.car.fuel < 20:
-#                 self.shopping("fuel")
-#             else:
-#                 self.to_repair()
-#
-#     def shopping(self, manage):
-#         if not self.car.drive():
-#             if self.car.fuel < 20:
-#                 manage = "fuel"
-#             else:
-#                 self.to_repair()
-#                 return
-#
-#         if manage == "fuel":
-#             print("I bought fuel")
-#             self.money -= 100
-#             self.car.fuel += 100
-#         elif manage == "food":
-#             print("Bought food")
-#             self.money -= 50
-#             self.home.food += 50
-#         elif manage == "delicacies":
-#             print("Hooray! Delicious!")
-#             self.gladness += 10
-#             self.satiety += 2
-#             self.money -= 15
-#
-#     def chill(self):
-#         self.gladness += 10
-#         self.home.mess += 5
-#
-#     def clean_home(self):
-#         self.gladness -= 5
-#         self.home.mess = 0
-#
-#     def to_repair(self):
-#         print("Repairing car")
-#         self.car.strength += 100
-#         self.money -= 50
-#
-#     def days_indexes(self, day):
-#         day_text = f" Today the {day} day of {self.name}'s life "
-#         print(f"{day_text:=^50}")
-#
-#         print(f"{self.name}'s indexes".center(50))
-#         print(f"Money: {self.money}")
-#         print(f"Satiety: {self.satiety}")
-#         print(f"Gladness: {self.gladness}")
-#
-#         print("Home indexes".center(50))
-#         print(f"Food: {self.home.food}")
-#         print(f"Mess: {self.home.mess}")
-#
-#         print(f"{self.car.brand} car indexes".center(50))
-#         print(f"Fuel: {self.car.fuel}")
-#         print(f"Strength: {self.car.strength}")
-#         print()
-#
-#     def is_alive(self):
-#         if self.gladness < 0:
-#             print("Depression…")
-#             return False
-#         if self.satiety < 0:
-#             print("Dead…")
-#             return False
-#         if self.money < -500:
-#             print("Bankrupt…")
-#             return False
-#         return True
-#
-#     def live(self, day):
-#         if not self.is_alive():
-#             return False
-#
-#         dice = random.randint(1, 4)
-#
-#         if self.home is None:
-#             print("Settled in the house")
-#             self.get_home()
-#
-#         if self.car is None:
-#             self.get_car()
-#             print(f"I bought a car {self.car.brand}")
-#
-#         if self.job is None:
-#             self.get_job()
-#             print(
-#                 f"I don't have a job, going to get a job "
-#                 f"{self.job.job} with salary {self.job.salary}"
-#             )
-#
-#         self.days_indexes(day)
-#
-#         if self.satiety < 20:
-#             print("I'll go eat")
-#             self.eat()
-#         elif self.gladness < 20:
-#             if self.home.mess > 15:
-#                 print("Too much mess, cleaning...")
-#                 self.clean_home()
-#             else:
-#                 print("Let`s chill!")
-#                 self.chill()
-#         elif self.money < 0:
-#             print("Start working")
-#             self.work()
-#         elif self.car.strength < 15:
-#             print("I need to repair my car")
-#             self.to_repair()
-#         elif dice == 1:
-#             print("Let`s chill!")
-#             self.chill()
-#         elif dice == 2:
-#             print("Start working")
-#             self.work()
-#         elif dice == 3:
-#             print("Cleaning time!")
-#             self.clean_home()
-#         elif dice == 4:
-#             print("Time for treats!")
-#             self.shopping("delicacies")
+# print("\n" + "=" * 50)
+# print("ДЕМОНСТРАЦИЯ РАБОТЫ")
+# print("=" * 50)
 #
 #
-# class Auto:
-#     def __init__(self, brand_list):
-#         self.brand = random.choice(list(brand_list))
-#         self.fuel = brand_list[self.brand]["fuel"]
-#         self.strength = brand_list[self.brand]["strength"]
-#         self.consumption = brand_list[self.brand]["consumption"]
-#
-#     def drive(self):
-#         if self.strength > 0 and self.fuel >= self.consumption:
-#             self.fuel -= self.consumption
-#             self.strength -= 1
-#             return True
-#         else:
-#             print("The car cannot move")
-#             return False
+# print(Fore.RED + "Красный текст")
+# print(Fore.GREEN + "Зелёный текст")
+# print(Fore.BLUE + "Синий текст")
+# print(Fore.YELLOW + "Жёлтый текст")
 #
 #
-# class House:
-#     def __init__(self):
-#         self.mess = 0
-#         self.food = 0
+# print(Back.CYAN + "Текст с голубым фоном")
+# print(Back.MAGENTA + "Текст с фиолетовым фоном")
 #
 #
-# class Job:
-#     def __init__(self, job_list):
-#         self.job = random.choice(list(job_list))
-#         self.salary = job_list[self.job]["salary"]
-#         self.gladness_less = job_list[self.job]["gladness_less"]
+# print(Style.BRIGHT + "Яркий текст")
+# print(Style.DIM + "Тусклый текст")
+# print(Style.NORMAL + "Обычный текст")
 #
+# print(Style.RESET_ALL + "Сброс стилей")
 #
-# job_list = {
-#     "Java developer": {"salary": 50, "gladness_less": 10},
-#     "Python developer": {"salary": 40, "gladness_less": 3},
-#     "C++ developer": {"salary": 45, "gladness_less": 25},
-#     "Rust developer": {"salary": 70, "gladness_less": 1},
-# }
-#
-# brands_of_car = {
-#     "BMW": {"fuel": 100, "strength": 100, "consumption": 6},
-#     "Lada": {"fuel": 50, "strength": 40, "consumption": 10},
-#     "Volvo": {"fuel": 70, "strength": 150, "consumption": 8},
-#     "Ferrari": {"fuel": 80, "strength": 120, "consumption": 14},
-# }
-#
-# nick = Human(name="Nick")
-#
-# for day in range(1, 8):
-#     if not nick.live(day):
-#         break
-# Если библиотека не установлена, раскомментируй строку ниже
-# pip install colorama
+# print("\nПрограмма завершена успешно!")
 
-import colorama
-import inspect
-from colorama import init, Fore, Back, Style
+import sqlite3
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import time
 
-init(autoreset=True)
+DB_NAME = "weather.db"
+CITY_URL = "https://wttr.in/Baku?format=j1"
 
-print("=" * 50)
-print("ИНТРОСПЕКЦИЯ БИБЛИОТЕКИ COLORAMA")
-print("=" * 50)
+def create_db():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS weather (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date_time TEXT,
+            temperature REAL
+        )
+    """)
+    conn.commit()
+    conn.close()
 
+def get_temperature():
+    response = requests.get(CITY_URL)
+    data = response.json()
+    temp = data["current_condition"][0]["temp_C"]
+    return float(temp)
 
-print("\nДокументация модуля:")
-print(colorama.__doc__)
+def insert_data(temp):
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    cursor.execute("INSERT INTO weather (date_time, temperature) VALUES (?, ?)", (now, temp))
+    conn.commit()
+    conn.close()
 
-print("\nОсновные атрибуты colorama:")
-print(dir(colorama))
+def main():
+    create_db()
+    while True:
+        try:
+            temperature = get_temperature()
+            insert_data(temperature)
+            print(f"Добавлено: {temperature} °C")
+        except Exception as e:
+            print("Ошибка:", e)
+        time.sleep(1800)
 
-print("\nИнформация о Fore:")
-print(dir(Fore))
-
-print("\nИнформация о Back:")
-print(dir(Back))
-
-print("\nИнформация о Style:")
-print(dir(Style))
-
-print("\n" + "=" * 50)
-print("ДЕМОНСТРАЦИЯ РАБОТЫ")
-print("=" * 50)
-
-
-print(Fore.RED + "Красный текст")
-print(Fore.GREEN + "Зелёный текст")
-print(Fore.BLUE + "Синий текст")
-print(Fore.YELLOW + "Жёлтый текст")
-
-
-print(Back.CYAN + "Текст с голубым фоном")
-print(Back.MAGENTA + "Текст с фиолетовым фоном")
-
-
-print(Style.BRIGHT + "Яркий текст")
-print(Style.DIM + "Тусклый текст")
-print(Style.NORMAL + "Обычный текст")
-
-print(Style.RESET_ALL + "Сброс стилей")
-
-print("\nПрограмма завершена успешно!")
+if __name__ == "__main__":
+    main()
